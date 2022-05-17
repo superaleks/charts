f54ab83dbbf8322828b5646da6047bbd8bce0e82
alukic <alukic@vmware.com>
2022-05-17 17:05:49 +0200

    Improved the tests some more
    Signed-off-by: alukic <alukic@vmware.com>

--- .vib/rabbitmq/cypress/cypress/integration/rabbitmq_spec.js
+++ .vib/rabbitmq/cypress/cypress/integration/rabbitmq_spec.js
@@ -61,7 +61,11 @@ it('allows adding a new queue and binding/unbinding it to the exchange', () => {
     cy.get('[name="name"]').type(`${queue.newQueue.name}${random}`);
     cy.get('#arguments_1_mfkey').type(queue.newQueue.argument1);
     cy.get('#arguments_1_mfvalue').type(queue.newQueue.argument2);
-    cy.contains('Add queue').click();
+    cy.contains('Add queue')
+      .scrollIntoView()
+      .should('be.enabled')
+      .click({ force: true });
+    cy.reload();
     cy.contains('table', `${queue.newQueue.name}${random}`);
   });
   cy.visit('#/exchanges');
